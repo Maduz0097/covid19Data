c71140b595ee1147b3d59f78bc8979066378675a
hashan malinda <85447795+hashan789@users.noreply.github.com>
2021-09-02 17:35:21 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,36 +1,45 @@
-import React,{ Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
+import axios from "axios";
+import React from "react";
 
-const api = axios.create({
-  baseURL : 'https://www.hpb.health.gov.lk/api/get-current-statistical'
-});
+import "./index.css";
 
-class App extends Component{
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
-  state = {
-    covid: []
-  }
+const baseURL = "https://www.hpb.health.gov.lk/api/get-current-statistical";
 
-  constructor() {
-    super();
-    api.get('/').then(res => {
-      console.log(res.data)
-      this.setState({covid:res.data})
-    })
-  }
+export default function App() {
+  const [post, setPost] = React.useState(null);
 
-  render(){
-    return(
-      <div className="App">
-        <h1>Daily corona report</h1>
-        <ul>
-          <li>{this.state.covid.map(person => <h2 key={person.update_date_time}>{person.global_deaths}</h2>)}</li>
-        </ul>
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  if (!post) return null;
+
+  return (
+   <div className="row">
+     <div><h1>Daily corona report<span> of {post.data.update_date_time}</span></h1></div>
+     <div className="col-lg-6 col-sm-12">
+      <h2 className="land">In sri lanka</h2>
+      <h2>new local cases :{post.data.local_new_cases}</h2>
+      <h2>number of individuals in hospitals:{post.data.local_total_number_of_individuals_in_hospitals}</h2>
+      <h2>local deaths :{post.data.local_deaths}</h2>
+      <h2>new local deaths :{post.data.local_new_deaths}</h2>
+     </div>
+      <div className="col-lg-6 col-sm-12">
+        <h2 className="land">In the world</h2>
+      <h2>Global deaths :{post.data.global_deaths}</h2>
+      <h2>New global deaths :{post.data.global_new_deaths}</h2>
+      <h2>New global cases :{post.data.global_new_cases}</h2>
+      <h2>Total global deaths :{post.data.global_total_cases}</h2>
+      <h2>Global recoverd persons :{post.data.global_recovered}</h2>
       </div>
-    )
-  }
+      <div className="col-lg-6 col-sm-12">
+      <h2>Total PCR testing :{post.data.total_pcr_testing_count}</h2>
+      </div>
+    </div>
+  );
 }
-
-export default App;
